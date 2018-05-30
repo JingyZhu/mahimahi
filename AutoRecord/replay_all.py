b4ccfa352e61c879772af38b9a6d87c9d181f6b0
JingyZhu <35050315+JingyZhu@users.noreply.github.com>
2018-05-30 14:03:51 -0400

    replay_all added

---
+++ AutoRecord/replay_all.py
@@ -0,0 +1,28 @@
+from subprocess import *
+import os
+import sys
+
+http = 'http://www.'
+https = 'https://www.'
+FNULL=open(os.devnull, 'w')
+web_list = open('weblist', 'r').read().split('\n')
+while web_list[-1] == "":
+    del web_list[-1]
+
+mmwebreplay = os.path.join(os.environ['mmpath'], 'usr/bin/mm-webreplay')
+mmlink = os.path.join(os.environ['mmpath'], 'usr/bin/mm-link')
+repo =  os.path.join(os.environ['mmpath'], 'tmp')
+
+i = 0
+for web in web_list:
+    web = web.split(',')
+    i += 1
+    sys.stdout.flush()
+    aurl = https + web[0] if bool(web[1]) else http + web[0]
+    url = http + web[0]
+    print(str(i) + " replay: " + aurl)
+    web = web[0]
+    try:
+        call([mmwebreplay, os.path.join(repo, web), mmlink, 'trace_file', 'trace_file', '--', 'python3', 'chrome.py', url], timeout=30, env=os.environ.copy(), stdout=FNULL, stderr=STDOUT)
+    except:
+        pass
