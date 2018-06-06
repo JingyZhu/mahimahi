a5d7aec4c2c30bb3e526369997cd706350d254a3
Ubuntu <ubuntu@ip-172-31-3-103.us-east-2.compute.internal>
2018-06-06 17:03:05 +0000

    analyze.py updated for two lines in a graph

--- PyScript/cache-control/analyze.py
+++ PyScript/cache-control/analyze.py
@@ -7,25 +7,31 @@ import random
 bfiles = os.listdir('bytes')
 cfiles = os.listdir('counts')
 bfracs = []
+bhttp = []
 cfracs = []
+chttp = []
 bstr = ''
 cstr = ''
 
 def parse_portion(filename):
     frac = []
+    http = []
     data = open(filename, 'r').read().split('\n')
     while data[-1] == '':
         del data[-1]
-    i = 0
     # print(filename + " " + str(len(data)))
-    while i < len(data):
+    for datus in data:
         try:
-            frac.append(float(data[i]))
+            datus = datus.split(' ')
+            if datus[0] == 'all:':
+                frac.append(float(datus[1))
+            elif datus[0] == 'http:':
+                http.append(float(datus[1]))
         except Exception as e:
             pass
-        i += 1
     frac.sort()
-    return None if frac == [] else median(frac)
+    datus.sort()
+    return None if frac == [] or http == [] else (median(frac), median(http))
 
 
 for bfile in bfiles:
@@ -44,26 +50,43 @@ bfiles = os.listdir('bytes')
 cfiles = os.listdir('counts')
 
 for bfile in bfiles:
-    frac = parse_portion(os.path.join('bytes',bfile))
+    frac, http = parse_portion(os.path.join('bytes',bfile))
     if frac != None:
         bfracs.append(frac)
+    if http != None:
+        bhttp.append(http)
+
 
 for cfile in cfiles:
-    frac = parse_portion(os.path.join('counts',cfile))
+    frac, http = parse_portion(os.path.join('counts',cfile))
     if frac != None:
         cfracs.append(frac)
+    if http != None:
+        chttp.append(http)
 
 bfracs.sort()
 cfracs.sort()
+bhttp.sort()
+chttp.sort()
 
 i = 1
 for bfrac in bfracs:
-    bstr += '[{}, {}],'.format( bfrac, i / len(bfracs) )
+    bstr += '[{}, {}, null],'.format( bfrac, i / len(bfracs) )
+    i += 1
+
+i = 1
+for bh in bhttp:
+    bstr += '[{}, null, {}],'.format( bh, i / len(bhttp) )
     i += 1
 
 i = 1
 for cfrac in cfracs:
-    cstr += '[{}, {}],'.format( cfrac, i / len(cfracs) )
+    cstr += '[{}, {}, null],'.format( cfrac, i / len(cfracs) )
+    i += 1
+
+i = 1
+for ch in chttp:
+    cstr += '[{}, null, {}],'.format( ch, i / len(chttp) )
     i += 1
 
 print(bstr)
